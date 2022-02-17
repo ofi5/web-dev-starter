a69326ed8af772b02f4f62f86c13edad12478c90
ofi5 <86188587+ofi5@users.noreply.github.com>
2022-02-17 21:56:27 +0530

    CSS practice

---
+++ js-demo/home.js
@@ -0,0 +1,4 @@
+let nums = prompt("enter percent", '');
+
+percentageOff(nums);
+
---
+++ js-demo/utils.js
@@ -0,0 +1,7 @@
+function showMessage(message) {
+    document.getElementById('message').textContent = message;
+}
+function percentageOff(percent) {
+    document.getElementById('percento').textContent = percent + "% OFF"
+
+}
